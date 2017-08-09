e9b8a4e4d884e7d84eba1b96cd103940fa92edcf
sorousharzani <soroush.arzani@yahoo.com>
2017-08-10 03:42:02 +0430

    User_Sign_Up

--- loginapp/views/login_views.py
+++ loginapp/views/login_views.py
@@ -1,9 +1,9 @@
 from rest_framework.response import Response
-
+from rest_framework.permissions import IsAuthenticated
 from loginapp.forms import DeviceForm
 from loginapp.models import Device, User, Token
 from musikhar.abstractions.views import IgnoreCsrfAPIView
-
+from loginapp.forms import SignupForm
 
 class DeviceSignUpView(IgnoreCsrfAPIView):
 
@@ -35,4 +35,22 @@ class DeviceSignUpView(IgnoreCsrfAPIView):
                 token = token_list.last()
 
             return Response(data={'token': token.key})
+
+
+class UserSignup(IgnoreCsrfAPIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        data = request.data
+        form = SignupForm(data)
+        user = request.user
+        if form.is_valid():
+           user.username = form.cleaned_data.get('username')
+           user.mobile = form.cleaned_data.get('moblie')
+           token = Token.objects.create(user=user)
+
+           user.save()
+
+           return Response(data={'token': token.key},status=status.HTTP_200_OK)
+
 
