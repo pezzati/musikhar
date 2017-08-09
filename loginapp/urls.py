5156525df48031c1bbb8eacf55113f6281bfd3a4
Peyman Ezzati <p.ezzati.ab@gmail.com>
2017-08-10 02:04:06 +0430

    device signup view

--- loginapp/urls.py
+++ loginapp/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
 from loginapp.views.edit_profile_views import ProfileView
+from loginapp.views.login_views import DeviceSignUpView
 
 urlpatterns = [
-    url(r'profile$', ProfileView.as_view(), name='user_profile'),
+    url(r'^profile$', ProfileView.as_view(), name='user_profile'),
+    url(r'^device_signup$', DeviceSignUpView.as_view(), name='device_signup_profile')
 ]
--- loginapp/views/login_views.py
+++ loginapp/views/login_views.py
@@ -1,12 +1,38 @@
-from django.http.response import HttpResponse
+from rest_framework.response import Response
 
-from loginapp.auth import if_authorized
-
-
-@if_authorized
-def test_view(request):
-    print(request.user)
-    return HttpResponse(content='OK')
+from loginapp.forms import DeviceForm
+from loginapp.models import Device, User, Token
+from musikhar.abstractions.views import IgnoreCsrfAPIView
 
 
+class DeviceSignUpView(IgnoreCsrfAPIView):
+
+    def post(self, request):
+        data = request.data
+        form = DeviceForm(data)
+        if form.is_valid():
+            udid = form.cleaned_data.get('udid')
+            device_type = form.cleaned_data.get('type')
+            try:
+                device = Device.objects.get(udid=udid, type=device_type)
+                user = device.user
+            except Device.DoesNotExist:
+                if request.user.is_authenticated():
+                    user = request.user
+                else:
+                    user = User.objects.create_user(username=udid, password=udid[:15])
+                Device.objects.create(
+                    udid=udid,
+                    type=device_type,
+                    os_version=form.cleaned_data.get('os_version'),
+                    user=user
+                )
+
+            token_list = Token.objects.filter(user=user)
+            if len(token_list) == 0:
+                token = Token.objects.create(user=user)
+            else:
+                token = token_list.last()
+
+            return Response(data={'token': token.key})
 
