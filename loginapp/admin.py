74056162b1859a9fc3e8d6a2ea2eaca12c863545
Peyman Ezzati <p.ezzati.ab@gmail.com>
2017-08-20 01:01:16 +0430

    some clearing

--- loginapp/admin.py
+++ loginapp/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from loginapp.models import User, Token , Artist
+from loginapp.models import User, Token
 
 
 admin.site.register(User)
 admin.site.register(Token)
-admin.site.register(Artist)
 
--- loginapp/models.py
+++ loginapp/models.py
@@ -1,6 +1,5 @@
 import os
 import binascii
-import datetime
 
 from datetime import timedelta
 
