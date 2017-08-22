aa2816bdc8704983542d856eb4b5a0f59c163348
Peyman Ezzati <p.ezzati.ab@gmail.com>
2017-08-22 17:43:02 +0430

    Artist View Set and Serializers

--- loginapp/serializers.py
+++ loginapp/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from loginapp.models import User, Device, Token
+from loginapp.models import User, Device, Token, Artist
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
@@ -27,3 +27,31 @@ class DeviceSerializer(serializers.ModelSerializer):
         instance.os_version = validated_data.get('os_version', instance.os_version)
         instance.save()
         return instance
+
+
+class ArtistSerializer(serializers.ModelSerializer):
+    poetried = serializers.SerializerMethodField(required=False)
+    composed = serializers.SerializerMethodField(required=False)
+    singed = serializers.SerializerMethodField(required=False)
+
+    def get_poetried(self, obj):
+        from karaoke.serializers import KaraokeSerializer
+        karaokes = obj.poetried.all()[:10]
+        serializer = KaraokeSerializer(karaokes, many=True)
+        return serializer.data
+
+    def get_composed(self, obj):
+        from karaoke.serializers import KaraokeSerializer
+        karaokes = obj.composed.all()[:10]
+        serializer = KaraokeSerializer(karaokes, many=True)
+        return serializer.data
+
+    def get_singed(self, obj):
+        from karaoke.serializers import KaraokeSerializer
+        karaokes = obj.singed.all()[:10]
+        serializer = KaraokeSerializer(karaokes, many=True)
+        return serializer.data
+
+    class Meta:
+        model = Artist
+        fields = ('name', 'user', 'poetried', 'composed', 'singed')
--- loginapp/urls.py
+++ loginapp/urls.py
@@ -1,10 +1,19 @@
 from django.conf.urls import url
+from rest_framework import routers
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from loginapp.views.edit_profile_views import ProfileView
 from loginapp.views.login_views import UserSignup, UserLogin
+from loginapp.viewsets import ArtistViewSet
+
+router = routers.DefaultRouter()
+router.register(r'artists', ArtistViewSet, base_name='get_artist')
 
 urlpatterns = [
     url(r'^profile$', ProfileView.as_view(), name='user_profile'),
     url(r'^signup$', UserSignup.as_view(), name='signup'),
     url(r'^login$', UserLogin.as_view(), name='login')
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns) + router.urls
+
