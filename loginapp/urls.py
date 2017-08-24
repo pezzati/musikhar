0fe661a1ed02ba638df5e635aacdabae73e29d80
Peyman Ezzati <p.ezzati.ab@gmail.com>
2017-08-24 15:02:45 +0430

    follow views

--- loginapp/urls.py
+++ loginapp/urls.py
@@ -1,10 +1,21 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from rest_framework import routers
+from rest_framework.urlpatterns import format_suffix_patterns
 
-from loginapp.views.edit_profile_views import ProfileView
+from loginapp.views.edit_profile_views import ProfileView, FollowingViewSet
 from loginapp.views.login_views import UserSignup, UserLogin, PasswordRecovery
 
+router = routers.DefaultRouter()
+router.register(r'follow', FollowingViewSet, 'follow_relations')
+
+profile_urls = [
+    url(r'^$', ProfileView.as_view(), name='user_profile'),
+]
+
+profile_urls = format_suffix_patterns(profile_urls) + router.urls
+
 urlpatterns = [
-    url(r'^profile$', ProfileView.as_view(), name='user_profile'),
+    url(r'^profile/', include(profile_urls, namespace='profile')),
     url(r'^signup$', UserSignup.as_view(), name='signup'),
     url(r'^login$', UserLogin.as_view(), name='login'),
     url(r'^recovery', PasswordRecovery.as_view(), name='password_recovery')
--- loginapp/views/edit_profile_views.py
+++ loginapp/views/edit_profile_views.py
@@ -1,12 +1,15 @@
-
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.decorators import list_route
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
+from loginapp.auth import CsrfExemptSessionAuthentication
+from loginapp.models import User, Follow
 from loginapp.serializers import UserProfileSerializer
 from loginapp.forms import ProfileForm
 from musikhar.abstractions.messages import ErrorMessaging
-from musikhar.abstractions.views import IgnoreCsrfAPIView
+from musikhar.abstractions.views import IgnoreCsrfAPIView, PermissionReadOnlyModelViewSet
 
 
 class ProfileView(IgnoreCsrfAPIView):
@@ -29,3 +32,39 @@ class ProfileView(IgnoreCsrfAPIView):
         serializer = UserProfileSerializer(instance=request.user)
         data = serializer.data
         return Response(data=data, status=status.HTTP_200_OK)
+
+
+class FollowingViewSet(PermissionReadOnlyModelViewSet):
+    serializer_class = UserProfileSerializer
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
+
+    def get_queryset(self):
+        return User.objects.none()
+
+    @list_route()
+    def followings(self, request):
+        followings = request.user.get_following()
+        return self.do_pagination(queryset=followings)
+
+    @list_route()
+    def followers(self, request):
+        followers = request.user.get_followers()
+        return self.do_pagination(queryset=followers)
+
+    @list_route(methods=['post'])
+    def follow(self, request):
+        data = request.data
+        followed = data.get('followed')
+        if followed:
+            try:
+                followed_user = User.objects.get(username=followed)
+                Follow.objects.create(followed=followed_user, follower=request.user)
+                return Response(status=status.HTTP_200_OK)
+            except User.DoesNotExist:
+                # TODO error msg
+                return Response(status=status.HTTP_404_NOT_FOUND)
+
+        # TODO error msg
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
