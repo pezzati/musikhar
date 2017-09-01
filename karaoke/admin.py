0c03a4d17f64a605176ac5833daedfddcb734503
Peyman Ezzati <p.ezzati.ab@gmail.com>
2017-09-01 11:51:55 +0430

    admin sample for karaoke

--- karaoke/admin.py
+++ karaoke/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 
 from karaoke.models import Karaoke, Line, Genre
 
-admin.site.register(Karaoke)
+
+@admin.register(Karaoke)
+class KaraokeAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'created_date',
+        'genre',
+    )
+
+    search_fields = (
+        'name',
+    )
+
+
 admin.site.register(Line)
 admin.site.register(Genre)
