004e53f65585ce6679872cbf2bf710e5f6eecdba
sorousharzani <soroush.arzani@yahoo.com>
2017-09-01 16:21:57 +0200

    AdminPanel_Created

--- karaoke/admin.py
+++ karaoke/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from karaoke.models import Karaoke, Line, Genre
+from karaoke.models import Karaoke, Line, Genre, Post
 
 
 @admin.register(Karaoke)
@@ -9,12 +9,62 @@ class KaraokeAdmin(admin.ModelAdmin):
         'name',
         'created_date',
         'genre',
+        'rate',
+        'rate_count',
+        'cover_photo',
+        'poem',
+        'genre',
+        'composer',
+        'singer',
+
     )
 
     search_fields = (
         'name',
+        'genre',
+        'singer',
+
     )
 
 
-admin.site.register(Line)
-admin.site.register(Genre)
+@admin.register(Line)
+class LineAdmin(admin.ModelAdmin):
+    list_display = (
+        'karaoke',
+        'text',
+        'start_time',
+        'end_time'
+    )
+
+    search_fields = (
+        'karaoke'
+    )
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'parent'
+    )
+
+    search_fields = (
+        'name'
+    )
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'name',
+        'recorded_file',
+        'like_state',
+        'karaoke',
+        'created_date'
+    )
+
+    search_fields = (
+        'user',
+        'name'
+    )
