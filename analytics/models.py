dfe85deca6cf8c3e3d47fd7f5961cabb81da6b3d
Peyman Ezzati <p.ezzati.ab@gmail.com>
2017-09-05 15:32:40 +0430

    Post and ownership and beyond

--- analytics/models.py
+++ analytics/models.py
@@ -1,21 +1,16 @@
 from django.db import models
 from django.utils import timezone
 
-from karaoke.models import Karaoke
+from karaoke.models import Post
 from loginapp.models import User
 
 
 class Like(models.Model):
-    KARAOKE_LIKE = 'karaoke'
-    TYPE_CHOICES = (
-        (KARAOKE_LIKE, 'Karaoke'),
-    )
 
     user = models.ForeignKey(User)
-    karaoke = models.ForeignKey(Karaoke, null=True, blank=True)
+    post = models.ForeignKey(Post, null=True, blank=True)
     time = models.DateTimeField(auto_now=timezone.now)
-    type = models.CharField(max_length=20, choices=TYPE_CHOICES, default=KARAOKE_LIKE)
 
     def __str__(self):
         if self.type == Like.KARAOKE_LIKE:
-            return '{}-{}:{}'.format(self.user.username, self.type, self.karaoke)
+            return '{}-{}:{}'.format(self.user.username, self.type, self.post)
--- karaoke/serializers.py
+++ karaoke/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 
-from karaoke.models import Karaoke, Post, Line, Genre, Poem
+from karaoke.models import Karaoke, Post, Genre, Poem
 from loginapp.serializers import ArtistSerializer
 
 
@@ -38,26 +38,53 @@ class GenreSerializer(serializers.ModelSerializer):
         fields = ('link', 'files_link', 'name', 'children')
 
 
-class LineSerializer(serializers.ModelSerializer):
+# class LineSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Line
+#         fields = ('text', 'start_time', 'end_time')
+
+
+class PostSerializer(serializers.ModelSerializer):
+    type = serializers.SerializerMethodField(required=False, read_only=True)
+    content = serializers.SerializerMethodField(required=False, read_only=True)
+
+    def get_type(self, obj):
+        return obj.subclass_type
+
+    def get_content(self, obj):
+        if obj.subclass_type == Poem.KARAOKE_TYPE:
+            return KaraokeSerializer(instance=obj.karaoke,
+                                     context={'caller': Karaoke, 'request': self.context.get('request')}).data
+        if obj.subclass_type == Poem.POEM_TYPE:
+            return PoemSerializer(instance=obj.poem,
+                                  context={'caller': Poem, 'request': self.context.get('request')}).data
+
     class Meta:
-        model = Line
-        fields = ('text', 'start_time', 'end_time')
+        model = Post
+        fields = (
+            'name',
+            'desc',
+            'cover_photo',
+            'created_date',
+            'type',
+            'content'
+        )
 
 
 class PoemSerializer(serializers.ModelSerializer):
     poet = ArtistSerializer(required=False, many=False)
     link = serializers.SerializerMethodField(required=False, read_only=True)
-    lyrics = serializers.SerializerMethodField(required=False)
+    # lyrics = serializers.SerializerMethodField(required=False)
 
     def get_link(self, obj):
         return 'http://{}{}{}'.format(self.context.get('request').domain, reverse('songs:get-poem-list'), obj.id)
 
-    def get_lyrics(self, obj):
-        # if self.context.get('detailed'):
-        lines = obj.lyrics()
-        serializered = LineSerializer(lines, many=True)
-        return serializered.data
-        # return []
+    # def get_lyrics(self, obj):
+    #     # if self.context.get('detailed'):
+    #     lines = obj.lyrics()
+    #     serializered = LineSerializer(lines, many=True)
+    #     return serializered.data
+    #     # return []
 
     class Meta:
         model = Poem
@@ -65,20 +92,27 @@ class PoemSerializer(serializers.ModelSerializer):
             'name',
             'poet',
             'link',
-            'lyrics'
+            'text',
+            'desc',
+            'cover_photo',
+            'created_date',
         )
 
 
 class KaraokeSerializer(serializers.ModelSerializer):
     link = serializers.SerializerMethodField(required=False, read_only=True)
+    like = serializers.SerializerMethodField(required=False, read_only=True)
     poet = ArtistSerializer(many=False, required=False)
     composer = ArtistSerializer(many=False, required=False)
     singer = ArtistSerializer(many=False, required=False)
     genre = SingleGenreSerializer(many=False, required=False)
-    poem = PoemSerializer(many=False, required=False)
+    related_poem = PoemSerializer(many=False, required=False)
 
     def get_link(self, obj):
         return 'http://{}{}{}'.format(self.context.get('request').domain, reverse('songs:get-karaoke-list'), obj.id)
+
+    def get_like(self, obj):
+        return obj.like_set.count()
 
     def to_representation(self, instance):
         self.context['caller'] = self.Meta.model
@@ -90,14 +124,15 @@ class KaraokeSerializer(serializers.ModelSerializer):
             'link',
             'name',
             'file',
-            'rate',
-            'rate_count',
-            'cover_photo',
+            'like',
             'poet',
             'genre',
             'composer',
             'singer',
-            'poem',
+            'related_poem',
+            'desc',
+            'cover_photo',
+            'created_date',
         )
 
 
