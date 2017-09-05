867599a783b4a05308c53cb86f47396bf3120865
Peyman Ezzati <p.ezzati.ab@gmail.com>
2017-09-05 16:55:43 +0430

    favorite model

--- analytics/models.py
+++ analytics/models.py
@@ -12,5 +12,14 @@ class Like(models.Model):
     time = models.DateTimeField(auto_now=timezone.now)
 
     def __str__(self):
-        if self.type == Like.KARAOKE_LIKE:
-            return '{}-{}:{}'.format(self.user.username, self.type, self.post)
+        return '{}-{}:{}'.format(self.user.username, self.post.subclass_type, self.post)
+
+
+class Favorite(models.Model):
+
+    user = models.ForeignKey(User)
+    post = models.ForeignKey(Post, null=True, blank=True)
+    time = models.DateTimeField(auto_now=timezone.now)
+
+    def __str__(self):
+        return '{}-{}:{}'.format(self.user.username, self.post.subclass_type, self.post)
