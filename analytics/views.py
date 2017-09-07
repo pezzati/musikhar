a065c588d374141addf3a80777c23dbb0eb70446
sorousharzani <soroush.arzani@yahoo.com>
2017-09-07 14:52:40 +0200

    Updated

--- analytics/views.py
+++ analytics/views.py
@@ -1,13 +1,17 @@
+
+from rest_framework.decorators import list_route, detail_route
+from analytics.models import Like, Favorite
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.decorators import list_route
-
+from rest_framework.response import Response
+from rest_framework import status
+from karaoke.models import Post
 from rest_framework.permissions import IsAuthenticated
-
 from loginapp.auth import CsrfExemptSessionAuthentication
 from loginapp.models import User
 from loginapp.serializers import UserProfileSerializer
-
-from musikhar.abstractions.views import PermissionReadOnlyModelViewSet
+from musikhar.abstractions.views import  PermissionReadOnlyModelViewSet
+from musikhar.utils import Errors
 
 
 class LikeViewSet(PermissionReadOnlyModelViewSet):
@@ -18,10 +22,31 @@ class LikeViewSet(PermissionReadOnlyModelViewSet):
     def get_queryset(self):
         return User.objects.none()
 
-    @list_route()
-    def like_get(self, request):
-        liked = request.user.get_like
-        return self.do_pagination(queryset=liked)
+    @detail_route()
+    def full(self, request, pk):
+        like = Like.objects.get(id=pk)
+        serialized = self.serializer_class(instance=like, context={'request': self.request.user.get_like(), 'detailed': True})
+        return self.do_pagination(queryset=serialized)
+
+    @list_route(methods=['post'])
+    def like(self, request):
+        data = request.data
+        liked = data.get('like')
+        liked_user = data.get('user')
+        liked_post = data.get('post')
+
+        if liked:
+            try:
+                like_user = User.objects.get(username=liked_user)
+                like_post = Post.objects.get(name=liked_post)
+                Like.objects.create(post=like_post, user=like_user)
+                return Response(status=status.HTTP_200_OK)
+            except User.DoesNotExist:
+                errors = Errors.get_errors(Errors, error_list=['User_Not_Found'])
+                return Response(data=errors, status=status.HTTP_404_NOT_FOUND)
+
+        errors = Errors.get_errors(Errors, error_list=['Missing_Form'])
+        return Response(data=errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class FavoriteViewSet(PermissionReadOnlyModelViewSet):
@@ -32,7 +57,8 @@ class FavoriteViewSet(PermissionReadOnlyModelViewSet):
     def get_queryset(self):
         return User.objects.none()
 
-    @list_route()
-    def favorite_get(self, request):
-        favored = request.user.get_favorite
-        return self.do_pagination(queryset=favored)
+    @detail_route()
+    def full(self, request, pk):
+        favored = Favorite.objects.get(id=pk)
+        serialized = self.serializer_class(instance=favored, context={'request': self.request.user.get_favorite(), 'detailed': True})
+        return self.do_pagination(queryset=serialized)
