b22fc8a275ba122c52543a5c3c2c950eeb69de66
Peyman Ezzati <p.ezzati.ab@gmail.com>
2017-09-19 15:29:47 +0430

    ALLOWED_HOSTS

--- musikhar/server_settings/production.py
+++ musikhar/server_settings/production.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '!kb!fbs77#30kwu-2m23_7m6cnd8-$z(&&ag&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['77.238.122.16']
 
 
 # Application definition
