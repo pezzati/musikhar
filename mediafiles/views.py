c360edb5eb26f7971b085aa35a866f822c8caa02
Peyman Ezzati <p.ezzati.ab@gmail.com>
2017-09-22 20:48:33 +0330

    fix content type for avatars

--- mediafiles/views.py
+++ mediafiles/views.py
@@ -51,6 +51,7 @@ def get_file(request):
     response = HttpResponse()
 
     if file_category == 'avatars':
+        response['Content-Type'] = 'image/jpg'
         response['X-Accel-Redirect'] = uri
         return response
 
