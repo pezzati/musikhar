3f74611e7a2bd1826000c5ffa6b0a7f5a475a9d0
Peyman Ezzati <p.ezzati.ab@gmail.com>
2017-09-23 13:47:57 +0330

    full path link

--- mediafiles/serializers.py
+++ mediafiles/serializers.py
@@ -10,6 +10,9 @@ class MediaFileSerializer(MySerializer):
     link = serializers.SerializerMethodField(required=False)
 
     def get_link(self, obj):
+        if obj.resource_type == MediaFile.LOCAL_RESOURCE and \
+                self.context.get('request') and self.context.get('request') is not None:
+            return 'http://{}{}'.format(self.context.get('request').domain, obj.__str__())
         return obj.__str__()
 
     class Meta:
