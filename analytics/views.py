f4e353225f11e81c4ecc85b632bec9005f823ad9
Peyman Ezzati <p.ezzati.ab@gmail.com>
2017-09-24 15:39:16 +0330

    TagSearch

--- analytics/views.py
+++ analytics/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from analytics.searchs import TagSearch
+from analytics.serializers import TagSerializer
+from loginapp.auth import CsrfExemptSessionAuthentication
+from musikhar.abstractions.views import PermissionReadOnlyModelViewSet
+
+
+class TagViewSet(PermissionReadOnlyModelViewSet):
+    search_class = TagSearch
+    serializer_class = TagSerializer
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
+
+    def create(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def update(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
