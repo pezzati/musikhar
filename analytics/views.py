fed1d911d1db87df36c37f29d3dee7a5852dbd82
Peyman Ezzati <p.ezzati.ab@gmail.com>
2017-11-14 18:47:14 +0330

    remove comments

--- analytics/views.py
+++ analytics/views.py
@@ -1,37 +1,15 @@
 from django.shortcuts import redirect
 
-# from rest_framework.decorators import detail_route
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
 from analytics.searchs import TagSearch
 from analytics.serializers import TagSerializer, BannerSerializer, NotificationSerializer
 from analytics.models import Banner
-# from karaoke.models import Post
 from loginapp.auth import CsrfExemptSessionAuthentication
 from musikhar.abstractions.views import PermissionReadOnlyModelViewSet
-# from musikhar.utils import Errors
-# from analytics.serializers import LikeSerializer, FavoriteSerializer
 
-
-# class LikeViewSet(PermissionReadOnlyModelViewSet):
-#     serializer_class = LikeSerializer
-#     permission_classes = (IsAuthenticated,)
-#     authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
-#
-#     def get_queryset(self):
-#         return Like.objects.none()
-
-
-# class FavoriteViewSet(PermissionReadOnlyModelViewSet):
-#     serializer_class = FavoriteSerializer
-#     permission_classes = (IsAuthenticated,)
-#     authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
-#
-#     def get_queryset(self):
-#         return self.request.user.favorite_set.all()
 
 class TagViewSet(PermissionReadOnlyModelViewSet):
     search_class = TagSearch
@@ -39,23 +17,11 @@ class TagViewSet(PermissionReadOnlyModelViewSet):
     permission_classes = (IsAuthenticated,)
     authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
 
-    # def create(self, request, *args, **kwargs):
-    #     return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
-    #
-    # def update(self, request, *args, **kwargs):
-    #     return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
-
 
 class BannerViewSet(PermissionReadOnlyModelViewSet):
     serializer_class = BannerSerializer
     permission_classes = (IsAuthenticated,)
     authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
-
-    # def create(self, request, *args, **kwargs):
-    #     return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
-    #
-    # def update(self, request, *args, **kwargs):
-    #     return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def get_queryset(self):
         return Banner.active_banners()
@@ -73,12 +39,6 @@ class NotificationViewSet(PermissionReadOnlyModelViewSet):
     serializer_class = NotificationSerializer
     permission_classes = (IsAuthenticated,)
     authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
-
-    def create(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
-
-    def update(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def get_queryset(self):
         return self.request.user.events.all()
