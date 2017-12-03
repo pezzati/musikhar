87065d86d88e5c9de175a199a07b83a0ce842128
Peyman Ezzati <p.ezzati.ab@gmail.com>
2017-12-03 18:44:56 +0330

    verification model, send mail/sms when they have changed

--- loginapp/models.py
+++ loginapp/models.py
@@ -27,12 +27,19 @@ class User(AbstractUser):
     gender = models.IntegerField(choices=GenderTypes, default=male)
     birth_date = models.DateTimeField(null=True, blank=True)
     image = models.FileField(upload_to=get_avatar_path, null=True, blank=True)
-    is_signup = models.BooleanField(default=False)
     country = models.CharField(max_length=50, null=True, blank=True)
+
     mobile = models.CharField(max_length=11, null=True, blank=True)
+    mobile_confirmed = models.BooleanField(default=False)
+    email_confirmed = models.BooleanField(default=False)
+
     bio = models.CharField(max_length=120, blank=True, null=True)
     referred_by = models.ForeignKey('self', null=True, blank=True, related_name='referrers')
     is_public = models.BooleanField(default=True)
+
+    point = models.IntegerField(default=0)
+    premium_time = models.DateField(null=True, blank=True)
+    is_premium = models.BooleanField(default=False)
 
     genres = models.ManyToManyField('karaoke.Genre', blank=True)
 
@@ -50,8 +57,22 @@ class User(AbstractUser):
     def send_sms_recovery_password(self):
         send_sms(self, msg={'msg': 'some msg'})
 
+    def send_mobile_verification(self):
+        self.verification_set.filter(type=Verification.SMS_CODE).delete()
+        self.mobile_confirmed = False
+        self.save(update_fields=['mobile_confirmed'])
+        verification = Verification.objects.create(user=self)
+        send_sms(self, msg={'msg': 'here is your code {}'.format(verification.code)})
+
     def send_email_recovery_password(self):
         send_email(self, msg={'msg': 'some msg'})
+
+    def send_email_verification(self):
+        self.verification_set.filter(type=Verification.EMAIL_CODE).delete()
+        self.email_confirmed = False
+        self.save(update_fields=['email_confirmed'])
+        verification = Verification.objects.create(user=self)
+        send_email(self, msg={'msg': 'here is your code {}'.format(verification.code)})
 
     def get_followers(self):
         return User.objects.filter(id__in=self.followers.values_list('follower'))
@@ -113,6 +134,34 @@ class User(AbstractUser):
             system_user.set_password(raw_password=settings.SYSTEM_USER['password'])
             system_user.save()
             return
+
+
+class Verification(models.Model):
+    SMS_CODE = 'sms'
+    EMAIL_CODE = 'email'
+    TYPE_CHOICES = (
+        (SMS_CODE, 'sms code'),
+        (EMAIL_CODE, 'email code')
+    )
+    code = models.CharField(max_length=6)
+    user = models.ForeignKey(User)
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES, default=SMS_CODE)
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '<{}, {}>'.format(self.user.username, self.code)
+
+    @staticmethod
+    def generate_token(length=6):
+        return binascii.hexlify(os.urandom(length)).decode()
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        self.code = self.generate_token()
+        super(Verification, self).save(force_insert=force_insert,
+                                       force_update=force_update,
+                                       using=using,
+                                       update_fields=update_fields)
 
 
 class Follow(models.Model):
