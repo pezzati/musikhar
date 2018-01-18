a8ca9fd13bd2eac716ad58759f1961003663d0f4
Peyman Ezzati <p.ezzati.ab@gmail.com>
2018-01-18 19:52:16 +0330

    Post Search fixed

--- karaoke/searchs.py
+++ karaoke/searchs.py
@@ -1,9 +1,9 @@
-from karaoke.models import Song, Genre
+from karaoke.models import Genre, Post
 from musikhar.abstractions.search import ModelSearch
 
 
 class PostSearch(ModelSearch):
-    model = Song
+    model = Post
     model_has_tags = True
     search_fields = ('name', 'description')
 
