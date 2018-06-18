96cb69ddfb5fff29173dba4a4663676f0f7e9921
Peyman Ezzati <pezzati@ce.sharif.edu>
2018-06-19 02:02:42 +0430

    signup with google

--- loginapp/views/login_views.py
+++ loginapp/views/login_views.py
@@ -176,3 +176,37 @@ class Verify(IgnoreCsrfAPIView):
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
         return Response(status=status.HTTP_200_OK)
+
+
+GOOGLE_CLIENT_ID = '243773746715-p421f9lui80f3mhvc8mhu188q4lg8sa6.apps.googleusercontent.com'
+
+
+class SignupGoogle(IgnoreCsrfAPIView):
+    def post(self, request):
+        from google.oauth2 import id_token
+        from google.auth.transport import requests
+
+        token = request.data.get('token')
+
+        try:
+            # Specify the CLIENT_ID of the app that accesses the backend:
+            idinfo = id_token.verify_oauth2_token(token, requests.Request(), GOOGLE_CLIENT_ID)
+
+            # Or, if multiple clients access the backend server:
+            # idinfo = id_token.verify_oauth2_token(token, requests.Request())
+            # if idinfo['aud'] not in [CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]:
+            #     raise ValueError('Could not verify audience.')
+
+            if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+                raise ValueError('Wrong issuer.')
+
+            # If auth request is from a G Suite domain:
+            # if idinfo['hd'] != GSUITE_DOMAIN_NAME:
+            #     raise ValueError('Wrong hosted domain.')
+
+            # ID token is valid. Get the user's Google Account ID from the decoded token.
+            userid = idinfo['sub']
+            return Response(status=status.HTTP_200_OK)
+        except Exception as e:
+            print(str(e))
+            return Response(status=status.HTTP_400_BAD_REQUEST)
