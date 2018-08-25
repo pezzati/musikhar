3345b0c73aeb4cbcd046fb738d86c0617bc4e826
Peyman Ezzati <pezzati@ce.sharif.edu>
2018-08-25 10:53:20 +0430

    change staging base dir and project path

--- musikhar/server_settings/staging.py
+++ musikhar/server_settings/staging.py
@@ -13,9 +13,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = '/web/production/'
-PROJECT_PATH = '/web/production/musikhar'
-PROJECT_ROOT = '/web/production/musikhar'
+BASE_DIR = '/web/staging/'
+PROJECT_PATH = '/web/staging/musikhar'
+PROJECT_ROOT = '/web/staging/musikhar'
 
 
 # Quick-start development settings - unsuitable for production
