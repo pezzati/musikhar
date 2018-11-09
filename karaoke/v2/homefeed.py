30c5aaf6fb166e34c6bf1521214e7822d9ea14b0
Peyman Ezzati <pezzati@ce.sharif.edu>
2018-11-09 18:31:53 +0330

    home_feed cache

--- karaoke/v2/homefeed.py
+++ karaoke/v2/homefeed.py
@@ -1,3 +1,6 @@
+import ast
+import logging
+
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
@@ -5,12 +8,24 @@ from rest_framework.reverse import reverse
 from karaoke.models import Feed, Genre, Post
 from karaoke.serializers import PostSerializer
 from musikhar.abstractions.views import IgnoreCsrfAPIView
+from musikhar.utils import conn
+
+error_logger = logging.getLogger("error")
 
 
 class HomeFeedV2(IgnoreCsrfAPIView):
     permission_classes = (IsAuthenticated,)
 
     def get(self, request):
+        raw_data = conn().get('home_feed')
+        if raw_data:
+            try:
+                return Response(ast.literal_eval(raw_data.decode('utf-8')))
+            except Exception as e:
+                error_logger.info('[CACHE_RESPONSE_HOME_FFED] ERROR: {}, request: {}, raw_data: {}'.format(str(e),
+                                                                                                 request.get_full_path(),
+                                                                                                 raw_data))
+
         res = list()
         feeds = Feed.objects.all()
         feed_link_pattern = 'http://' + request.domain + '{}{}/karaokes'
@@ -31,4 +46,7 @@ class HomeFeedV2(IgnoreCsrfAPIView):
                     data=PostSerializer(genre.post_set.all()[:10], many=True, context={'caller': Post, 'request': request}).data
                 )
             )
+
+        conn().set(name='home_feed', value=res, ex=86400)
+
         return Response(data=res)
