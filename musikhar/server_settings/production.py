a583bff76bc6bd544c294fdb105546880b96ad8b
Peyman Ezzati <pezzati@ce.sharif.edu>
2018-12-19 16:27:24 +0330

    add test.canto-app.ir tp allowsed hosts

--- musikhar/server_settings/production.py
+++ musikhar/server_settings/production.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '!kb!fbs77#30kwu-2m23_7m6cnd8-$z(&&ag&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['canto-app.ir', 'cantoapp.ir']
+ALLOWED_HOSTS = ['canto-app.ir', 'cantoapp.ir', 'test.canto-app.ir']
 
 REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
 # Application definition
