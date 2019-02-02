2d29244d105fd2d6700d4983a231ccd633bfceae
Peyman Ezzati <pezzati@ce.sharif.edu>
2019-02-02 12:34:32 +0330

    covers

--- mediafiles/models.py
+++ mediafiles/models.py
@@ -55,7 +55,9 @@ class MediaFile(models.Model):
         if self.resource_type == MediaFile.LOCAL_RESOURCE:
             return '{}{}'.format(settings.MEDIA_URL, self.file.name)
         else:
-            return self.path.replace('storage.backtory.com', 'cantoapp.ir')
+            if 'covers' not in self.path:
+                return self.path.replace('storage.backtory.com', 'cantoapp.ir')
+            return self.path
 
     def get_download_path(self):
         """
